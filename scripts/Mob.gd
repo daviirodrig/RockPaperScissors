07a2bf6b1c4e4e92c91f052784deb59c8e691cd0
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2023-01-31 19:43:39 -0300

    change assets directory

--- scripts/Mob.gd
+++ scripts/Mob.gd
@@ -12,7 +12,7 @@ func _ready():
 func set_sprite():
 	var sprite = get_node("Area2D/Sprite")
 	var keys = Globals.mob_types.keys()
-	var texture = load("res://sprites/%s.png" % keys[type].to_lower())
+	var texture = load("res://assets/sprites/%s.png" % keys[type].to_lower())
 	sprite.texture = texture
 
 
@@ -81,7 +81,7 @@ func check_collide(collider: Mob):
 func play_sound(name: String):
 	var hit_audio = AudioStreamPlayer.new()
 	hit_audio.connect("finished", get_node("/root/Main"), "_on_Audio_finished", [hit_audio])
-	var stream = load("res://sfx/%s.mp3" % name.to_lower())
+	var stream = load("res://assets/sfx/%s.mp3" % name.to_lower())
 	hit_audio.stream = stream
 	get_node("/root/Main").add_child(hit_audio)
 	hit_audio.play()
@@ -92,8 +92,8 @@ func _physics_process(_delta):
 
 
 func _on_Area2D_input_event(_viewport, event, _shape_idx):
-	var outline_shader = load("res://shaders/outline.shader")
-	var glow_shader = load("res://shaders/glow.shader")
+	var outline_shader = load("res://assets/shaders/outline.shader")
+	var glow_shader = load("res://assets/shaders/glow.shader")
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1:
 		if Globals.controlling_node == self:
 			load_shader_on_node(self, outline_shader)
