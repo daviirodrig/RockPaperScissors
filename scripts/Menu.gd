92f257222fb0e21fbf23e490008fbd57b6a5748e
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2023-01-31 19:57:27 -0300

    add only one script to Menu

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -1,5 +1,9 @@
-extends Button
+extends Control
 
+
+func _ready():
+	$Subtitle/AnimationPlayer.play("New Anim")
 
 func _on_StartButton_pressed():
 	get_tree().change_scene("res://scenes/Main.tscn")
+
--- scripts/Subtitle.gd
+++
@@ -1,5 +0,0 @@
-extends Label
-
-
-func _ready():
-	$AnimationPlayer.play("New Anim")
