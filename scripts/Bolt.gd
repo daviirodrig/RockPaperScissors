f94316628a96ce4773aad7a5570964df18a7aa03
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2023-02-09 04:28:01 -0300

    set rainbow shader on bolt pickup

--- scripts/Bolt.gd
+++ scripts/Bolt.gd
@@ -2,12 +2,19 @@ extends Area2D
 
 
 func _on_Bolt_body_entered(body):
-	if "Mob" in body.name:
-		self.queue_free()
-		body.speed = 15
-		var timer = Timer.new()
-		timer.wait_time = 5
-		timer.one_shot = true
-		timer.autostart = true
-		timer.connect("timeout", get_node("/root/SignalManager"), "_on_Bolt_timeout", [body, timer])
-		get_node("/root/Main").add_child(timer)
+	if !("Mob" in body.name): return
+
+	self.queue_free()
+	body.speed = 25
+	var sprite = body.get_node("Area2D/Sprite")
+	sprite.material = ShaderMaterial.new()
+	sprite.material.shader = load("res://assets/shaders/rainbow.shader")
+	sprite.material.set_shader_param("speed", 3)
+
+	var timer = Timer.new()
+	timer.wait_time = 5
+	timer.one_shot = true
+	timer.autostart = true
+	timer.connect("timeout", get_node("/root/SignalManager"), "_on_Bolt_timeout", [body, timer])
+
+	get_node("/root/Main").add_child(timer)
--- scripts/SignalManager.gd
+++ scripts/SignalManager.gd
@@ -4,6 +4,7 @@ extends Node
 func _on_Bolt_timeout(mob, timer):
 	if is_instance_valid(mob):
 		mob.speed = 10
+		mob.load_shader_on_node(mob, load("res://assets/shaders/outline.shader"))
 	timer.queue_free()
 
 func _on_Audio_finished(audio):
